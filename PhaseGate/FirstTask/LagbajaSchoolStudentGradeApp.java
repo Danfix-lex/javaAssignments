917213b230580fd27f2af1c8af177a4168e09edc
Danfix-lex <danielojolex@gmail.com>
2024-12-03 12:41:19 +0100

    Phase Gate

--- PhaseGate/FirstTask/LagbajaSchoolStudentGradeApp.java
+++ PhaseGate/FirstTask/LagbajaSchoolStudentGradeApp.java
@@ -9,21 +9,24 @@ public class LagbajaSchoolStudentGradeApp {
 	    System.out.println("Welcome to Lagbaja School Student's Grade App");
 	    System.out.println();
 	    
-	    int[] studentNumber = new int [10];
-	    int[] subjectNumber = new int [10];
+	    int[] studentNumber = new int [4];
+	    int[] subjectNumber = new int [4];
+	    int[] studentScore = new int [4];
 	
 	    System.out.println("Welcome Teacher please enter the number of student or enter (-1) if you have finished entering the number of students: ");
 	    int studentCount = 1;
       for (int i = 0; i < 10; i++) {
+      System.out.print("Do you wish to start/continue input any digit or input -1 to end or skip to the next process: ");
+      int studentUserInput = input.nextInt();
       System.out.println("Enter the student number in accordance to the below student number (e.g 1, 2, 3 etc...)");
       System.out.println();
-      System.out.print("Student's "+studentCount+": ");
-      studentNumber[i] = input.nextInt();
-      if (studentNumber[i] == -1) {
+      if (studentUserInput == -1 ||studentCount == 5 ) {
         System.out.println("You can now proceed to the subject aspect!!");
         System.out.println();
         break;
         }
+      System.out.print("Student's "+studentCount+": ");
+      studentNumber[i] = input.nextInt();
       studentCount ++;
       System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>");
       System.out.println("Saved successfully");
@@ -32,37 +35,36 @@ public class LagbajaSchoolStudentGradeApp {
 	    System.out.println("Welcome Teacher please enter the number of subject or enter (-1) if you have finished entering the number of subjects: ");
 	    int subjectCount = 1;
       for (int j = 0; j < 10; j++) {
+      System.out.print("Do you wish to start/continue input any digit or input -1 to end or skip to the next process: ");
+      int subjectUserInput = input.nextInt();
       System.out.println("Enter the subject number in accordance to the below subject number (e.g 1, 2, 3 etc...)");
       System.out.println();
-      System.out.print("Subject's "+subjectCount+": ");
-      subjectNumber[j] = input.nextInt();
-      if (subjectNumber[j] == -1) {
-        System.out.println("You can now proceed to the scores aspect!!");
+      if (subjectUserInput == -1 || subjectCount == 5 ) {
+        System.out.println("You can now proceed to the subject aspect!!");
         System.out.println();
         break;
-      }
+        }
+      System.out.print("Subject's "+subjectCount+": ");
+      subjectNumber[j] = input.nextInt();
       subjectCount ++;
       System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>");
       System.out.println("Saved successfully");
 	}
 	
-      for (int j = 0; j < 10; j++) {
-      System.out.printf("Entering score for student  the subject number in accordance to the below subject number (e.g 1, 2, 3 etc...)");
-      System.out.println();
-      System.out.print("Subject's "+subjectCount+": ");
-      subjectNumber[j] = input.nextInt();
-      if (subjectNumber[j] == -1) {
-        System.out.println("You can now proceed to the scores aspect!!");
-        System.out.println();
-        break;
+      for (int k = 0; k < studentNumber.length+1; k++) {
+        System.out.printf("Entering score for student %d%n", k+1, studentNumber[k+1]);
+        for (int l = 0; l < subjectNumber.length+1; l++) {
+          System.out.printf("Enter score for subject %d:", l+1);
+          System.out.println();
+          studentScore[l] = input.nextInt();
+          System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>");
+          System.out.println("Saved successfully");
       }
-      subjectCount ++;
-      System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>");
-      System.out.println("Saved successfully");
+    }
+      
 	}
 	
 	
 	
 	
 	}
-}
